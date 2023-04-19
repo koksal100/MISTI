8cdb5329c06812b16963292cff07ef9ea8eaf8f2
koksal100 <119877414+koksal100@users.noreply.github.com>
2023-04-19 15:22:18 +0300

    Txt File Reader

--- Card.java
+++ Card.java
@@ -1,22 +1,25 @@
 package SE116PROJECT;
 
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class Card {
 	private int value;
 	private String suit;
 	private String cardface;
 	private String cardName;
 
-
 	Card(String suit, String cardface) {
 		setCardface(cardface);
 		setSuit(suit);
-        setValue();
+		setValue();
 
 	}
 
-	public String getCardName(){
-		return suit+ " " +cardface;
-}
+	public String getCardName() {
+		return suit + " " + cardface;
+	}
+
 	public String getSuit() {
 		return suit;
 	}
@@ -39,6 +42,34 @@ public class Card {
 	}
 
 	public void setValue() {
-		// GET VALUES FROM TXT FILE
+		Scanner reader = null;
+		String[] informations = new String[2];
+		boolean isAssigned = false;
+		int i = 0;
+		try {
+			reader = new Scanner(Paths.get("points.txt"));
+			while (reader.hasNextLine()) {
+				informations = reader.nextLine().split(" ");
+				if ((this.getSuit() + this.getCardface()).equals(informations[0])
+						|| ("*" + this.getCardface()).equals(informations[0])
+						|| (this.getSuit() + "*").equals(informations[0])) {
+					this.value = Integer.parseInt(informations[1]);
+					isAssigned = true;
+					break;
+				}
+
+			}
+			if (!isAssigned) {
+				this.value = 1;
+			}
+
+		} catch (Exception e) {
+
+		} finally {
+			if (reader != null) {
+				reader.close();
+			}
+		}
+
 	}
 }
