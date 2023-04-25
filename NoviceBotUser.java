e8be1d90e3585ea36ea83d1141c4e2e9c856cd0f
İrem Nur Şener <119794427+iremnursener@users.noreply.github.com>
2023-04-25 13:17:50 +0300

    RegularBot and NoviceBot completed.

--- NoviceBotUser.java
+++ NoviceBotUser.java
@@ -1,16 +1,22 @@
 package SE116PROJECT;
 
+import java.util.Random;
+
 public class NoviceBotUser extends AbstractUser {
 
-	NoviceBotUser(String name) {
-		super(name);
-	}
-	NoviceBotUser() {
+    NoviceBotUser(String name) {
+        super(name);
+    }
 
-	}
-	
-	public Card findBestCardToPlay() {
-		Card card= new Card();
-		return card;
-	}
+    NoviceBotUser() {
+
+    }
+
+    public Card findBestCardToPlay() {
+        Random rand = new Random();
+        int randIndex = rand.nextInt(getCurrentCards().size());
+
+
+        return getCurrentCards().get(randIndex);
+    }
 }
