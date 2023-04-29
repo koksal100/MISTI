3ecdfe400d2573c8be52bd6fcd38f8c78cb02b3f
koksal100 <119877414+koksal100@users.noreply.github.com>
2023-04-29 17:53:57 +0300

    Txt file bugs have been fixed

--- Card.java
+++ Card.java
@@ -6,100 +6,102 @@ import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Card {
-	private int value;
-	private String suit;
-	private String cardface;
-	private String cardName;
+    private int value;
+    private String suit;
+    private String cardface;
+    private String cardName;
 
-	private static String fileAdress=getFileName();
-	Card(String suit, String cardface) {
-		setCardface(cardface);
-		setSuit(suit);
-		setValue(fileAdress);
+    private static String fileAdress = getFileName();
 
-	}
-	Card() {
+    Card(String suit, String cardface) {
+        setCardface(cardface);
+        setSuit(suit);
+        setValue(fileAdress);
+
+    }
+
+    Card() {
 
 
-	}
+    }
 
-	public String getCardName() {
-		return suit + " " + cardface;
-	}
+    public String getCardName() {
+        return suit + " " + cardface;
+    }
 
-	public String getSuit() {
-		return suit;
-	}
+    public String getSuit() {
+        return suit;
+    }
 
-	public void setSuit(String suit) {
+    public void setSuit(String suit) {
 
-		this.suit = suit;
-	}
+        this.suit = suit;
+    }
 
-	public String getCardface() {
-		return cardface;
-	}
+    public String getCardface() {
+        return cardface;
+    }
 
-	public void setCardface(String cardface) {
-		this.cardface = cardface;
-	}
+    public void setCardface(String cardface) {
+        this.cardface = cardface;
+    }
 
-	public int getValue() {
-		return value;
-	}
+    public int getValue() {
+        return value;
+    }
 
-	public void setValue(String fileAdress) {
-		Scanner reader = null;
-		String[] informations = new String[2];
-		boolean isAssigned = false;
-		int i = 0;
-		try {
+    public void setValue(String fileAdress) {
+        Scanner reader = null;
+        String[] informations = new String[2];
+        boolean isAssigned = false;
+        int i = 0;
+        try {
 
-			reader = new Scanner(fileAdress);
-			while (reader.hasNextLine()) {
-				informations = reader.nextLine().split(" ");
-				if ((this.getSuit() + this.getCardface()).equals(informations[0])
-						|| ("*" + this.getCardface()).equals(informations[0])
-						|| (this.getSuit() + "*").equals(informations[0])) {
-					this.value = Integer.parseInt(informations[1]);
-					isAssigned = true;
-					break;
-				}
+            reader = new Scanner(Paths.get(fileAdress));
+            while (reader.hasNextLine()) {
+                informations = reader.nextLine().split(" ");
+                if ((this.getSuit() + this.getCardface()).equals(informations[0])
+                        || ("*" + this.getCardface()).equals(informations[0])
+                        || (this.getSuit() + "*").equals(informations[0])) {
+                    this.value = Integer.parseInt(informations[1]);
+                    isAssigned = true;
+                    break;
+                }
 
-			}
-			if (!isAssigned) {
-				this.value = 1;
-			}
+            }
+            if (!isAssigned) {
+                this.value = 1;
+            }
 
-		} catch (Exception e) {
+        } catch (Exception e) {
 
-		} finally {
-			if (reader != null) {
-				reader.close();
-			}
-		}
+        } finally {
+            if (reader != null) {
+                reader.close();
+            }
+        }
 
-	}
+    }
 
-	public static String getFileName(){
-		Scanner scan = new Scanner(System.in);
-		Path path;
-		String fileName="";
-		boolean isAssigned=false;
-		int i =0;
-		while(!isAssigned){
-			System.out.println("PLEASE GIVE THE ADDRESS OF THE FILE:");
-			fileName= scan.nextLine();
-			path = Paths.get(fileName);
-			if(Files.exists(path)){
-				isAssigned=true;
-				System.out.println("FILE IS FOUND!");
-				break;
-			}else{
-				System.out.println("FILE DOES NOT EXIST! TRY IT AGAIN.");
-			}
-		}
+    public static String getFileName() {
+        Scanner scan = new Scanner(System.in);
+        Path path;
+        String fileName = "";
+        boolean isAssigned = false;
+        int i = 0;
+        while (!isAssigned) {
+            System.out.println("PLEASE GIVE THE ADDRESS OF THE FILE:");
+            fileName = scan.nextLine();
+            path = Paths.get(fileName);
+            if (Files.exists(path) && !fileName.equals("")) {
+                isAssigned = true;
+                System.out.println("FILE IS FOUND!");
+                break;
+            } else {
+                System.out.println("FILE DOES NOT EXIST! TRY IT AGAIN.");
+            }
+        }
 
-		return fileName;
-	}
+        return fileName;
+    }
 }
